54bedafc3f6e60ef0916bb719311d594fd8317a9
king8fisher <140904760+king8fisher@users.noreply.github.com>
2023-09-19 12:17:52 -0400

    demo: useEffect with useSignalEffect on island

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,4 +1,6 @@
-import type { Signal } from "@preact/signals";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { useSignalEffect, type Signal } from "@preact/signals";
+import { useEffect } from "https://esm.sh/v128/preact@10.15.1/hooks/src/index.js";
 import { Button } from "../components/Button.tsx";
 
 interface CounterProps {
@@ -6,6 +8,29 @@ interface CounterProps {
 }
 
 export default function Counter(props: CounterProps) {
+  let timeout = 0;
+
+  if (IS_BROWSER) {
+    useSignalEffect(() => {
+      const _subscribe = props.count.value;
+      if (_subscribe <= 3) {
+        console.log('skip');
+        return;
+      }
+      console.log(_subscribe);
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        console.log('timeout')
+      }, 500);
+    });
+    useEffect(() => {
+      console.log('effect');
+      return () => {
+        console.log('cleanup');
+      }
+    })
+  }
+
   return (
     <div class="flex gap-8 py-6">
       <Button onClick={() => props.count.value -= 1}>-1</Button>
